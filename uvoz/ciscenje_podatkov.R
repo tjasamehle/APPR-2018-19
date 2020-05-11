951011b211d88f6e6e1fd767c5bcfa8f5ffb050b
Tjaša Mehle <tjasa.mehle@hotmail.com>
2020-05-11 09:39:54 +0200

    dokoncno urejene tabele

--- uvoz/ciscenje_podatkov.R
+++ uvoz/ciscenje_podatkov.R
@@ -6,12 +6,12 @@ library(gsubfn)
 
 
 #Čiščenje wiki tabele 
-tabela_wiki$Rank <- NULL
-tabela_wiki$'Prize in pound' <- NULL
-sl <- locale(date_format = "%AD", decimal_mark=",", grouping_mark=".")
-tabela_wiki[['Date']] <- parse_number(tabela_wiki[['Date']], locale=sl)
+#tabela_wiki$Rank <- NULL
+#tabela_wiki$'Prize in pound' <- NULL
+#sl <- locale(date_format = "%AD", decimal_mark=",", grouping_mark=".")
+#tabela_wiki[['Date']] <- parse_number(tabela_wiki[['Date']], locale=sl)
 #tabela_wiki[['Prize in euro']]  Da bo numerično
-colnames(tabela_wiki) <- c("Leto","Nagrada(€)","Država")
+#colnames(tabela_wiki) <- c("Leto","Nagrada(€)","Država")
 
 
 
@@ -28,17 +28,18 @@ locena <- razpredelnica2 %>% separate('Številka', c("Stevilka", "Stevilka2"), "
 prvih_pet <- subset(locena, select = -c(Stevilka2))
 prvih_pet$Tip <- c(rep('prvih_pet', 351))
 prvih_pet <- prvih_pet %>% separate('Stevilka', c("Stevilka", "Stevilka2","Stevilka3","Stevilka4","Stevilka5"), ",");
-prvih_pet <- prvih_pet%>% gather(Nepomembno,Stevilka, Stevilka:Stevilka5)
+prvih_pet <- prvih_pet %>% gather(Nepomembno,Stevilka, Stevilka:Stevilka5)
 prvih_pet <- subset(prvih_pet, select = -Nepomembno)
 
-zadnje_dve<-subset(locena, select = -Stevilka)
+zadnje_dve <- subset(locena, select = -Stevilka)
 zadnje_dve$Tip <- c(rep('zadnje_dve', 351))
-zadnje_dve<- zadnje_dve %>% separate('Stevilka2', c("Stevilka1", "Stevilka2"), ",");
-zadnje_dve<- zadnje_dve%>% gather(Nepomembno,Stevilka, Stevilka1:Stevilka2)
+zadnje_dve <- zadnje_dve %>% separate('Stevilka2', c("Stevilka1", "Stevilka2"), ",");
+zadnje_dve <- zadnje_dve%>% gather(Nepomembno,Stevilka, Stevilka1:Stevilka2)
 zadnje_dve <- subset(zadnje_dve, select = -Nepomembno)
 
 tabela2 <-rbind(prvih_pet, zadnje_dve)
 tabela2$Stevilka <- as.numeric(tabela2$Stevilka)
+
 #3.RAZPREDELNICA(DATUM,DOBITEK, VREDNOST)
 
 razpredelnica3 <- tabela[c(1,2,5)]
@@ -46,12 +47,12 @@ colnames(razpredelnica3)<- c("Datum","Dobitek","Vrednost")
 razpredelnica3 <-razpredelnica3 %>% filter(Vrednost != '--')  #kjer ni bilo dobitka damo ven
 #write_csv(razpredelnica3, "razpredelnica.csv", na="")
 razpredelnica <- read_csv("razpredelnica.csv")
-razpredelnica3<- razpredelnica %>% separate(!!'Vrednost', c("JACKPOT", "5+1"), "-");
-razpredelnica3<- razpredelnica3%>% gather(Dobitek,Vrednost, "JACKPOT":"5+1")
+razpredelnica3 <- razpredelnica %>% separate(!!'Vrednost', c("JACKPOT", "5+1"), "-");
+razpredelnica3 <- razpredelnica3%>% gather(Dobitek,Vrednost, "JACKPOT":"5+1")
 tabela3 <- razpredelnica3 %>% filter(Vrednost != "")
 sl <- locale("sl", decimal_mark=",", grouping_mark=".")
 tabela3[['Vrednost']] <- parse_number(tabela3[['Vrednost']], na="-", locale=sl)
-tabela3$Vrednost<- as.numeric(tabela3$Vrednost)
+tabela3$Vrednost <- as.numeric(tabela3$Vrednost)
 
 
 #4.RAZPREDELNICA(DATUM, DOBITEK, STEVILO, DRŽAVA)
@@ -61,21 +62,47 @@ tabela4 <-tabela4 %>% filter(Stevilo != '--')
 #write_csv(tabela4, "razpredelnica2.csv", na="")
 razpredelnica4 <- read_csv("razpredelnica2.csv")
 tabelaa<- razpredelnica4 %>% separate('Stevilo', c("JACKPOT", "5+1"), "-")
-tabelaa<-tabelaa %>% separate('Drzava', c("dr_JACKPOT", "dr_5"), "-")
-tabelaa<- tabelaa%>% gather(Dobitek,Stevilo, "JACKPOT":"5+1")
-tabelaa<-tabelaa %>% filter(Stevilo != "");
-tabelaa<- tabelaa%>% gather(Nepomembno,Drzava, "dr_JACKPOT":"dr_5")
-tabelaa<-tabelaa %>% filter(Drzava != "");
-tabelaa<-subset(tabelaa, select = -Nepomembno)
+tabelaaa<-razpredelnica4 %>% separate('Drzava', c("dr_JACKPOT", "dr_5"), "-")
+
+jackpot<-tabelaa[c(1,2)]
+jackpot$Drzava<-tabelaaa$dr_JACKPOT
+jackpot <- jackpot %>% drop_na(JACKPOT)
+jackpot <- jackpot %>% filter(JACKPOT != "")
+colnames(jackpot) <- c("datum","stevilo","drzava")
+jackpot <- jackpot %>% separate('drzava', c("a", "b","c","d"), ",", extra="merge")
+jackpot <- jackpot %>% gather(Nepomembno,drzava, "a":"d")
+jackpot <- jackpot %>% drop_na(drzava)
+jackpot <- subset(jackpot, select = -Nepomembno)
+c <- parse_number(jackpot$drzava)      #parse_number-vzame število, če ni je NA
+is.na_replace_1 <- c                            
+is.na_replace_1[is.na(is.na_replace_1)] <- 1 
+jackpot$stevilo <- is.na_replace_1
+jackpot$dobitek <- c(rep('jackpot', times=60))
+
+pet<-tabelaa[c(1,3)]
+pet$Drzava <- tabelaaa$dr_5
+colnames(pet) <- c("datum","stevilo","drzava")
+pet <- pet %>%filter(stevilo != "")
+pet <- pet %>% separate('drzava', c("a", "b","c","d","e", "f","g","h"), ",", extra="merge")
+pet <- pet%>% gather(Nepomembno,drzava, "a":"h")
+pet <- pet %>%drop_na(drzava)
+pet <- subset(pet, select = -Nepomembno)
+c <- parse_number(pet$drzava)
+is.na_replace_1 <- c                            
+is.na_replace_1[is.na(is.na_replace_1)] <- 1 
+pet$stevilo<-is.na_replace_1
+pet <- pet %>%filter(drzava != "")
+pet$dobitek <- c(rep('5+1', times=623))
 
 
-tabelaa$Stevilo <- as.numeric(tabelaa$Stevilo)
-u <-tabelaa %>% filter(Stevilo >1);
-u<-u %>% separate('Drzava', c("a", "b","c","d","e","f","g","h"), ",", extra="merge")
-u<- u%>% gather(Nepomembno,Drzava, "a":"h")
-u<-u %>% drop_na(Drzava);
-u<-subset(u, select = -Nepomembno)
+tabela_koncna <- rbind(jackpot,pet)
+tabela_koncna$drzava <- gsub("[[:digit:]]x", "", tabela_koncna$drzava)
 
-#drzava <- tabelaa$Drzava %>% strapplyc("([[:alpha:]]+)")
-#stevilo
+
+#RAZPREDELNICA ZA SLOVENIJO (dodan tudi kraj)
+tabela_koncna <- tabela_koncna %>% separate('drzava', c("drzava","kraj"), "\\(", extra="merge")
+tabela_koncna$kraj<- gsub("\\)", "", tabela_koncna$kraj)
+slovenija<- tabela_koncna %>%filter(drzava == "Slovenija")
+
+tabela4 <- subset(tabela_koncna, select = - kraj)
 
